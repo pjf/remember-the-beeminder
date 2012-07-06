f96fe769a6e5bdfd4e6d7cecb1030f6982cf1e3b
Paul Fenwick <pjf@perltraining.com.au>
2012-07-06 14:25:56 -0700

    Basic auth and finding inbox.

---
+++ lib/RTBM.pm
@@ -0,0 +1,48 @@
+package RTBM;
+
+use v5.10.1;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::Method::Signatures;
+use Config::Tiny;
+use Carp;
+
+extends 'WebService::RTMAgent';
+
+# After we build our object, auto-init with RTM.
+
+sub BUILD {
+    my ($self) = @_;
+
+    my $config = Config::Tiny->read("$ENV{HOME}/.rtbmrc");
+    $config or die "Can't read ~/.rtbmrc config file";
+
+    $self->api_key(    $config->{RTBM}{api_key}    );
+    $self->api_secret( $config->{RTBM}{api_secret} );
+
+    $self->init;
+
+    return;
+}
+
+method lists {
+    my $lists = $self->lists_getList
+        or croak $self->error;
+
+    # Unpack lists from our data structure.
+    return $lists->{lists}[0]{list};
+}
+
+method find_list (Str $listname) {
+    my $lists = $self->lists;
+
+    foreach my $list (@$lists) {
+        return $list if lc($list->{name}) eq lc($listname);
+    }
+
+    croak "$listname not found";
+}
+
+1;
