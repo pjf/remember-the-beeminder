52b741659b7577f1758f71d60e3ed00e56305a73
Paul Fenwick <pjf@perltraining.com.au>
2012-07-30 00:57:35 -0700

    Basic scoring. We don't yet cache which tasks have been scored already.

--- lib/RTBM.pm
+++ lib/RTBM.pm
@@ -11,6 +11,10 @@ use Carp;
 
 extends 'WebService::RTMAgent';
 
+# Shouldn't really be rw, but I'm lazy and this lets me set it
+# in BUILD.
+has '_config' => (is => 'rw', isa => 'Config::Tiny');
+
 # After we build our object, auto-init with RTM.
 
 sub BUILD {
@@ -19,6 +23,10 @@ sub BUILD {
     my $config = Config::Tiny->read("$ENV{HOME}/.rtbmrc");
     $config or die "Can't read ~/.rtbmrc config file";
 
+    # Save our config for later use.
+    $self->_config($config);
+
+    # Configure our RTM settings.
     $self->api_key(    $config->{RTBM}{api_key}    );
     $self->api_secret( $config->{RTBM}{api_secret} );
 
@@ -75,6 +83,33 @@ method get_done (Str $date) {
     my $done = $self->tasks_getList("filter=completed:$date");
 
     return $done->{tasks}[0]{list};
+}
+
+# Given a task and its list, returns how many points it's worth
+method score(HashRef $task, Str $list) {
+    state $scoring_table = $self->_config->{Scores};
+
+    my $score = $scoring_table->{DEFAULT};  # We start with the default score
+
+    $score = $scoring_table->{$list} // $score;   # Use list score, if exists
+
+    # Tags override lists, and we take the highest scoring tag on a task.
+
+    my $tag_score;
+
+    # Walk through my tags, remember score if applicable.
+
+    foreach my $tag (@{ $task->{tags}[0]{tag} || [] }) {
+        no warnings 'uninitialized';
+        if ($scoring_table->{$tag} >= $tag_score) {
+            $tag_score = $scoring_table->{$tag};
+        }
+    }
+
+    $score = $tag_score // $score;  # Take tag score if it was found
+
+    return $score;
+
 }
 
 1;
